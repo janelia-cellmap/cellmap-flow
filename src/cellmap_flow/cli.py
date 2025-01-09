fc5a85ad55cfc7140df3a552024da85e07924ab4
davidackerman <ackermand@janelia.hhmi.org>
2025-01-08 21:40:36 -0500

    minimal working code for interactive inference across local or remote sources

---
+++ src/cellmap_flow/cli.py
@@ -0,0 +1,322 @@
+import argparse
+import subprocess
+import logging
+import neuroglancer
+import os
+import sys
+import signal
+
+import select
+import itertools
+
+logging.basicConfig()
+
+processes = []
+job_ids = []
+hosts = []
+security = "http"
+import subprocess
+
+neuroglancer.set_server_bind_address("0.0.0.0")
+
+
+def is_bsub_available():
+    try:
+        # Run 'which bsub' to check if bsub is available in PATH
+        result = subprocess.run(
+            ["which", "bsub"], stdout=subprocess.PIPE, stderr=subprocess.PIPE
+        )
+
+        if result.stdout:
+            return True
+        else:
+            return False
+    except Exception as e:
+        print("Error:", e)
+
+
+def cleanup(signum, frame):
+    print(f"Script is being killed. Received signal: {signum}")
+
+    if is_bsub_available():
+        # Run your command here
+        for job_id in job_ids:
+            print(f"Killing job {job_id}")
+            os.system(f"bkill {job_id}")
+    else:
+        for process in processes:
+            process.kill()
+    sys.exit(0)
+
+
+# Attach signal handlers
+signal.signal(signal.SIGINT, cleanup)  # Handle Ctrl+C
+signal.signal(signal.SIGTERM, cleanup)  # Handle termination
+
+
+def get_host_from_stdout(output):
+    # Print or parse the output line-by-line
+    if "Host name: " in output and f"Listening at: {security}://" in output:
+        host_name = output.split("Host name: ")[1].strip()
+        port = output.split(f"Listening at: {security}://0.0.0.0:")[1].split(" (")[0]
+
+        hosts.append(f"{security}://{host_name}:{port}")
+        print(f"{hosts=}")
+        return True
+    return False
+
+
+def parse_bpeek_output(job_id):
+    # Run bpeek to get the job's real-time output
+    command = f"bpeek {job_id}"
+
+    try:
+        # Process the output in real-time
+        while True:
+            process = subprocess.Popen(
+                command,
+                shell=True,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                text=True,
+            )
+
+            output = process.stdout.read()
+            error_output = process.stderr.read()
+            if (
+                output == ""
+                and process.poll() is not None
+                and f"Job <{job_id}> : Not yet started." not in error_output
+            ):
+                break  # End of output
+            if output:
+                # Print or parse the output line-by-line
+                if get_host_from_stdout(output):
+                    break
+                # Example: Parse a specific pattern (e.g., errors or warnings)
+                if "error" in output.lower():
+                    print(f"Error found: {output.strip()}")
+
+        # Capture any error output
+        error_output = process.stderr.read()
+        if error_output:
+            print(f"Error: {error_output.strip()}")
+
+    except Exception as e:
+        print(f"Error while executing bpeek: {e}")
+
+
+def submit_bsub_job(
+    job_name="my_job",
+):
+    if security == "https":
+        command = "gunicorn --certfile=host.cert --keyfile=host.key --bind 0.0.0.0:0 --workers 1 --threads 1 example_virtual_n5_generic:app"
+    else:
+        command = "gunicorn --bind 0.0.0.0:0 --workers 1 --threads 1 example_virtual_n5_generic:app"
+    # if security == "https":
+    #     command = "pixi run gunicorn --certfile=host.cert --keyfile=host.key --bind 0.0.0.0:0 --workers 1 --threads 1 example_virtual_n5_generic:app"
+    # else:
+    #     command = "pixi run gunicorn --bind 0.0.0.0:0 --workers 1 --threads 1 example_virtual_n5_generic:app"
+    # Get the current Conda environment
+    conda_env = os.environ.get("CONDA_DEFAULT_ENV")
+    if not conda_env:
+        raise EnvironmentError(
+            "No active Conda environment found. Activate a Conda environment and try again."
+        )
+    # Create the bsub command
+    bsub_command = [
+        "bsub",
+        "-J",
+        job_name,
+        # "-o",
+        # "/dev/stdout",
+        # "-e",
+        # "/dev/stderr",
+        "-P",
+        "cellmap",
+        "-q",
+        "gpu_h100",
+        "-gpu",
+        "num=1",
+        "bash",
+        "-c",
+        f"source ~/.bashrc && conda activate {conda_env} && hostname && {command}",
+    ]
+
+    # Submit the job
+    print("Submitting job with the following command:")
+
+    try:
+        result = subprocess.run(
+            bsub_command, capture_output=True, text=True, check=True
+        )
+        print("Job submitted successfully:")
+        print(result.stdout)
+    except subprocess.CalledProcessError as e:
+        print("Error submitting job:")
+        print(e.stderr)
+
+    return result
+
+
+def generate_neuroglancer_link(dataset_path, inference_dict):
+    # Create a new viewer
+    viewer = neuroglancer.UnsynchronizedViewer()
+
+    # Add a layer to the viewer
+    with viewer.txn() as s:
+        if ".zarr" in dataset_path:
+            filetype = "zarr"
+        elif ".n5" in dataset_path:
+            filetype = "n5"
+        else:
+            filetype = "precomputed"
+        if dataset_path.startswith("/"):
+            dataset_path = dataset_path.replace("/nrs/cellmap/", "/nrs/").replace(
+                "/groups/cellmap/cellmap/", "/dm11/"
+            )
+            s.layers["raw"] = neuroglancer.ImageLayer(
+                source=f"{filetype}://{security}://cellmap-vm1.int.janelia.org/{dataset_path}",
+            )
+        else:
+            s.layers["raw"] = neuroglancer.ImageLayer(
+                source=f"{filetype}://{dataset_path}",
+            )
+        colors = ["red", "green", "blue"]
+        color_cycle = itertools.cycle(colors)
+        for host, model in inference_dict.items():
+            color = next(color_cycle)
+            s.layers[model] = neuroglancer.ImageLayer(
+                source=f"n5://{host}/{model}",
+                shader=f"""#uicontrol invlerp normalized(range=[0, 255], window=[0, 255]);
+#uicontrol vec3 color color(default="{color}");
+void main(){{emitRGB(color * normalized());}}""",
+            )
+        print(viewer)  # neuroglancer.to_url(viewer.state))
+        while True:
+            pass
+
+
+def run_locally():
+    # Command to execute
+    command = [
+        # "pixi",
+        # "run",
+        "gunicorn",
+        # "--certfile=host.cert",
+        # "--keyfile=host.key",
+        "--bind",
+        "0.0.0.0:0",
+        "--workers",
+        "1",
+        "--threads",
+        "1",
+        "example_virtual_n5_generic:app",
+    ]
+
+    # Start the subprocess
+    process = subprocess.Popen(
+        command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True
+    )
+
+    # Use select to check for output without blocking
+    # NOTE: For some reason the output is considered stderr here but is stdout ion cluster
+    # try:
+    output = ""
+    while True:
+        # Check if there is data available to read from stdout and stderr
+        rlist, _, _ = select.select(
+            [process.stdout, process.stderr], [], [], 0.1
+        )  # Timeout is 0.1s
+
+        # Read from stdout if data is available
+        if process.stdout in rlist:
+            output += process.stdout.readline()
+        if get_host_from_stdout(output):
+            break
+
+        # Read from stderr if data is available
+        if process.stderr in rlist:
+            output += process.stderr.readline()
+        if get_host_from_stdout(output):
+            break
+        # Check if the process has finished and no more output is available
+        if process.poll() is not None and not rlist:
+            break
+    processes.append(process)
+    # except KeyboardInterrupt:
+    #     print("Process interrupted.")
+    # finally:
+    #     process.stdout.close()
+    #     process.stderr.close()
+    #     process.wait()  # Wait for the process to terminate
+
+
+def start_hosts(num_hosts=1):
+    if is_bsub_available():
+        for _ in range(num_hosts):
+            result = submit_bsub_job(job_name="example_job")
+            job_ids.append(result.stdout.split()[1][1:-1])
+        for job_id in job_ids:
+            parse_bpeek_output(job_id)
+    else:
+        run_locally()
+
+
+if __name__ == "__main__":
+    # parse input arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-d",
+        "--dataset_path",
+        type=str,
+        help="Data path, including scale",
+        required=True,
+    )
+    parser.add_argument(
+        "-s", "--scale", type=str, help="Scale to apply models", required=True
+    )
+    parser.add_argument(
+        "-m", "--models", type=str, help="Comma-separated list of models", required=True
+    )
+
+    args = parser.parse_args()
+    models = args.models.split(",")
+    scale = args.scale
+    dataset_path = args.dataset_path
+    if dataset_path.endswith("/"):
+        dataset_path = dataset_path[:-1]
+    print(f"Dataset: {dataset_path}, Scale: {scale}, Models: {models}")
+    logging.info("Starting hosts...")
+    print("starting_hosts")
+    start_hosts(num_hosts=len(models))
+    print("started hosts")
+
+    logging.info("Starting hosts completed!")
+    inference_dict = {}
+    if len(hosts) != len(models):
+        raise ValueError(
+            "Number of hosts and models should be the same, but something went wrong"
+        )
+
+    print(hosts, models)
+    for host, model in zip(hosts, models):
+        inference_dict[host] = f"{dataset_path}__{scale}__{model}"
+
+    generate_neuroglancer_link(dataset_path, inference_dict)
+# %%
+# import neuroglancer
+# import time
+
+# neuroglancer.set_server_bind_address("0.0.0.0")
+
+# viewer = neuroglancer.UnsynchronizedViewer()
+# with viewer.txn() as s:
+#     s.layers["raw"] = neuroglancer.ImageLayer(
+#         source="precomputed://gs://neuroglancer-janelia-flyem-hemibrain/emdata/clahe_yz/jpeg"
+#     )
+#     # s.layers["raw2"] = neuroglancer.LocalVolume()
+#     print(viewer)
+#     while True:
+#         pass
+# # %%
---
+++ src/cellmap_flow/example_virtual_n5_generic.py
@@ -0,0 +1,229 @@
+"""
+# Example Virtual N5
+
+Example service showing how to host a virtual N5,
+suitable for browsing in neuroglancer.
+
+Neuroglancer is capable of browsing N5 files, as long as you store them on
+disk and then host those files over http (with a CORS-friendly http server).
+But what if your data doesn't exist on disk yet?
+
+This server hosts a "virtual" N5.  Nothing is stored on disk,
+but neuroglancer doesn't need to know that.  This server provides the
+necessary attributes.json files and chunk files on-demand, in the
+"locations" (url patterns) that neuroglancer expects.
+
+For simplicity, this file uses Flask. In a production system,
+you'd probably want to use something snazzier, like FastAPI.
+
+To run the example, install a few dependencies:
+
+    conda create -n example-virtual-n5 -c conda-forge zarr flask flask-cors
+    conda activate example-virtual-n5
+
+Then just execute the file:
+
+    python example_virtual_n5.py
+
+Or, for better performance, use a proper http server:
+
+    conda install -c conda-forge gunicorn
+    gunicorn --bind 0.0.0.0:8000 --workers 8 --threads 1 example_virtual_n5:app
+
+You can browse the data in neuroglancer after configuring the viewer with the appropriate layer [settings][1].
+
+[1]: http://neuroglancer-demo.appspot.com/#!%7B%22dimensions%22:%7B%22x%22:%5B1e-9%2C%22m%22%5D%2C%22y%22:%5B1e-9%2C%22m%22%5D%2C%22z%22:%5B1e-9%2C%22m%22%5D%7D%2C%22position%22:%5B5000.5%2C7500.5%2C10000.5%5D%2C%22crossSectionScale%22:25%2C%22projectionScale%22:32767.999999999996%2C%22layers%22:%5B%7B%22type%22:%22image%22%2C%22source%22:%7B%22url%22:%22n5://http://127.0.0.1:8000%22%2C%22transform%22:%7B%22outputDimensions%22:%7B%22x%22:%5B1e-9%2C%22m%22%5D%2C%22y%22:%5B1e-9%2C%22m%22%5D%2C%22z%22:%5B1e-9%2C%22m%22%5D%2C%22c%5E%22:%5B1%2C%22%22%5D%7D%7D%7D%2C%22tab%22:%22rendering%22%2C%22opacity%22:0.42%2C%22shader%22:%22void%20main%28%29%20%7B%5Cn%20%20emitRGB%28%5Cn%20%20%20%20vec3%28%5Cn%20%20%20%20%20%20getDataValue%280%29%2C%5Cn%20%20%20%20%20%20getDataValue%281%29%2C%5Cn%20%20%20%20%20%20getDataValue%282%29%5Cn%20%20%20%20%29%5Cn%20%20%29%3B%5Cn%7D%5Cn%22%2C%22channelDimensions%22:%7B%22c%5E%22:%5B1%2C%22%22%5D%7D%2C%22name%22:%22colorful-data%22%7D%5D%2C%22layout%22:%224panel%22%7D
+"""
+
+# %%
+# NOTE: To generate host key and host cert do the following: https://serverfault.com/questions/224122/what-is-crt-and-key-files-and-how-to-generate-them
+# openssl genrsa 2048 > host.key
+# chmod 400 host.key
+# openssl req -new -x509 -nodes -sha256 -days 365 -key host.key -out host.cert
+# Then can run like this:
+# gunicorn --certfile=host.cert --keyfile=host.key --bind 0.0.0.0:8000 --workers 1 --threads 1 example_virtual_n5:app
+# NOTE: You will probably have to access the host:8000 separately and say it is safe to go there
+
+# %% load image zoo
+from bioimageio.core import load_description
+from bioimageio.core import predict  # , predict_many
+
+
+import argparse
+from http import HTTPStatus
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+import numpy as np
+import numcodecs
+from scipy import spatial
+from zarr.n5 import N5ChunkWrapper
+from funlib.persistence import open_ds
+from funlib.geometry import Roi
+import numpy as np
+from funlib.geometry import Coordinate
+from skimage.morphology import erosion
+from scipy.ndimage import binary_dilation
+
+
+# NOTE: Normally we would just load in run but here we have to recreate it to save time since our run has so many points
+import neuroglancer
+import socket
+import skimage
+from image_data_interface import ImageDataInterface
+import logging
+
+app = Flask(__name__)
+CORS(app)
+
+from bioimageio.core import Tensor
+from bioimagezoo_processor import process_chunk
+
+import socket
+
+# Get the hostname
+hostname = socket.gethostname()
+
+# Get the local IP address
+
+print(f"Host name: {hostname}", flush=True)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--debug", action="store_true")
+    parser.add_argument("-p", "--port", default=8000)
+    args = parser.parse_args()
+    app.run(
+        host="0.0.0.0",
+        # port=args.port,
+        debug=args.debug,
+        threaded=not args.debug,
+        use_reloader=args.debug,
+    )
+
+
+# OUTPUT_VOXEL_SIZE = [8 * 2**SCALE_LEVEL, 8 * 2**SCALE_LEVEL, 8 * 2**SCALE_LEVEL]
+
+
+# %%
+SCALE_LEVEL = None
+IDI_RAW = None
+OUTPUT_VOXEL_SIZE = None
+VOL_SHAPE_ZYX = None
+VOL_SHAPE = None
+VOL_SHAPE_ZYX_IN_BLOCKS = None
+VOXEL_SIZE = None
+BLOCK_SHAPE = None
+MAX_SCALE = None
+CHUNK_ENCODER = None
+EQUIVALENCES = None
+DS = None
+MODEL = None
+
+
+# determined-chimpmunk is edges
+# kind-seashell is mito
+# happy-elephant is cells
+@app.route("/<path:dataset>/attributes.json")
+def top_level_attributes(dataset):
+    if "__" not in dataset:
+        return jsonify({"n5": "2.1.0"}), HTTPStatus.OK
+
+    if not (dataset.startswith("gs://") or dataset.startswith("s3://")):
+        dataset = "/" + dataset
+
+    dataset_name, s, BMZ_MODEL_ID = dataset.split("__")
+
+    global OUTPUT_VOXEL_SIZE, BLOCK_SHAPE, VOL_SHAPE, CHUNK_ENCODER, IDI_RAW, MODEL
+    # SCALE_LEVEL = 2
+
+    print(dataset_name, s, BMZ_MODEL_ID)
+    SCALE_LEVEL = int(s[1:])
+    # BMZ_MODEL_ID = "determined-chipmunk"  # "happy-elephant"  # "determined-chipmunk"  # "kind-seashell"  # "affable-shark"
+    MODEL = load_description(BMZ_MODEL_ID)
+
+    # global SCALE_LEVEL, IDI_RAW, OUTPUT_VOXEL_SIZE, VOL_SHAPE_ZYX, VOL_SHAPE, VOL_SHAPE_ZYX_IN_BLOCKS, VOXEL_SIZE, BLOCK_SHAPE, MAX_SCALE, CHUNK_ENCODER
+    # SCALE_LEVEL = 1
+    # IDI_RAW = ImageDataInterface(f"/nrs/cellmap/data/jrc_mus-liver-zon-2/jrc_mus-liver-zon-2.zarr/recon-1/em/fibsem-uint8/s{SCALE_LEVEL}")
+    IDI_RAW = ImageDataInterface(f"{dataset_name}/s{SCALE_LEVEL}")
+    OUTPUT_VOXEL_SIZE = IDI_RAW.voxel_size
+
+    # %%
+    BLOCK_SHAPE = np.array([128, 128, 128, 9])
+    MAX_SCALE = 0
+
+    VOL_SHAPE_ZYX = np.array(IDI_RAW.shape)
+    VOL_SHAPE = np.array([*VOL_SHAPE_ZYX[::-1], 9])
+    VOL_SHAPE_ZYX_IN_BLOCKS = np.ceil(VOL_SHAPE_ZYX / BLOCK_SHAPE[:3]).astype(int)
+    VOXEL_SIZE = IDI_RAW.voxel_size
+
+    CHUNK_ENCODER = N5ChunkWrapper(np.uint8, BLOCK_SHAPE, compressor=numcodecs.GZip())
+
+    scales = [[2**s, 2**s, 2**s, 1] for s in range(MAX_SCALE + 1)]
+    attr = {
+        "pixelResolution": {
+            "dimensions": [*OUTPUT_VOXEL_SIZE, 1],
+            "unit": "nm",
+        },
+        "ordering": "C",
+        "scales": scales,
+        "axes": ["x", "y", "z", "c^"],
+        "units": ["nm", "nm", "nm", ""],
+        "translate": [0, 0, 0, 0],
+    }
+    return jsonify(attr), HTTPStatus.OK
+
+
+@app.route("/<path:dataset>/s<int:scale>/attributes.json")
+def attributes(dataset, scale):
+    attr = {
+        "transform": {
+            "ordering": "C",
+            "axes": ["x", "y", "z", "c^"],
+            "scale": [
+                *OUTPUT_VOXEL_SIZE,
+                1,
+            ],
+            "units": ["nm", "nm", "nm", ""],
+            "translate": [0.0, 0.0, 0.0, 0.0],
+        },
+        "compression": {"type": "gzip", "useZlib": False, "level": -1},
+        "blockSize": BLOCK_SHAPE[:].tolist(),
+        "dataType": "uint8",
+        "dimensions": VOL_SHAPE.tolist(),
+    }
+    return jsonify(attr), HTTPStatus.OK
+
+
+@app.route(
+    "/<path:dataset>/s<int:scale>/<int:chunk_x>/<int:chunk_y>/<int:chunk_z>/<int:chunk_c>/"
+)
+def chunk(dataset, scale, chunk_x, chunk_y, chunk_z, chunk_c):
+    """
+    Serve up a single chunk at the requested scale and location.
+
+    This 'virtual N5' will just display a color gradient,
+    fading from black at (0,0,0) to white at (max,max,max).
+    """
+    # assert chunk_c == 0, "neuroglancer requires that all blocks include all channels"
+    corner = BLOCK_SHAPE[:3] * np.array([chunk_z, chunk_y, chunk_x])
+    box = np.array([corner, BLOCK_SHAPE[:3]]) * OUTPUT_VOXEL_SIZE
+    roi = Roi(box[0], box[1])
+    print("about_to_process_chunk")
+    chunk = process_chunk(MODEL, IDI_RAW, roi)
+    print(chunk)
+    return (
+        # Encode to N5 chunk format (header + compressed data)
+        CHUNK_ENCODER.encode(chunk),
+        HTTPStatus.OK,
+        {"Content-Type": "application/octet-stream"},
+    )
+
+
+# # %%
+# import multiprocessing as mp
+
+if __name__ == "__main__":
+    #    mp.set_start_method("spawn")
+    main()
