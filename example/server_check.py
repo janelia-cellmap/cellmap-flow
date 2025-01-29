36842e56a11b62938d9a680b7cec9d5419465d9a
rhoadesScholar <rhoadesj@hhmi.org>
2025-01-29 16:13:07 -0500

    Fix model_setup04.py and server_check.py for improved readability and path handling

--- example/server_check.py
+++ example/server_check.py
@@ -6,14 +6,15 @@ from cellmap_flow.utils.data import (
     DaCapoModelConfig,
     ScriptModelConfig,
 )
+import os
 
 # %%
-# dataset = "/nrs/cellmap/data/jrc_mus-cerebellum-1/jrc_mus-cerebellum-1.zarr/recon-1/em/fibsem-uint8/s0"
-# script_path = "/groups/cellmap/cellmap/zouinkhim/cellmap-flow/example/model_setup04.py"
+# dataset = "/groups/cellmap/cellmap/ackermand/for_hideo/jrc_pri_neuron_0710Dish4/jrc_pri_neuron_0710Dish4.n5/em/fibsem-uint8/s0"
+# script_path = "/groups/cellmap/cellmap/zouinkhim/cellmap-flow/example/model_spec.py"
 # script_path = "/groups/cellmap/cellmap/zouinkhim/cellmap-flow/example/dacapo_run_retrieve.py"
 
-script_path = "/groups/cellmap/cellmap/zouinkhim/cellmap-flow/example/model_spec.py"
-dataset = "/groups/cellmap/cellmap/ackermand/for_hideo/jrc_pri_neuron_0710Dish4/jrc_pri_neuron_0710Dish4.n5/em/fibsem-uint8/s0"
+script_path = os.path.join(os.path.dirname(__file__), "model_setup04.py")
+dataset = "/nrs/cellmap/data/jrc_mus-cerebellum-1/jrc_mus-cerebellum-1.zarr/recon-1/em/fibsem-uint8/s0"
 
 model_config = ScriptModelConfig(script_path=script_path)
 server = CellMapFlowServer(dataset, model_config)
@@ -24,3 +25,5 @@ chunk_z = 2
 server._chunk_impl(None, None, chunk_x, chunk_y, chunk_z, None)
 
 print("Server check passed")
+
+# %%
