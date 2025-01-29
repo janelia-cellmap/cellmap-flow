16d7b2395a69394f2b154fbda219a39448240df5
rhoadesScholar <rhoadesj@hhmi.org>
2025-01-29 15:41:33 -0500

    Refactor server_check function to accept script path and dataset as arguments; add command-line interface for execution.

--- example/server_check.py
+++ example/server_check.py
@@ -1,20 +1,32 @@
-#%%
+# %%
 from cellmap_flow.server import CellMapFlowServer
-from cellmap_flow.utils.data import ModelConfig, BioModelConfig, DaCapoModelConfig, ScriptModelConfig
-#%%
-# dataset = "/nrs/cellmap/data/jrc_mus-cerebellum-1/jrc_mus-cerebellum-1.zarr/recon-1/em/fibsem-uint8/s0"
-# script_path = "/groups/cellmap/cellmap/zouinkhim/cellmap-flow/example/model_setup04.py"
-# script_path = "/groups/cellmap/cellmap/zouinkhim/cellmap-flow/example/dacapo_run_retrieve.py"
+from cellmap_flow.utils.data import (
+    ModelConfig,
+    BioModelConfig,
+    DaCapoModelConfig,
+    ScriptModelConfig,
+)
+import argparse
 
-script_path = "/groups/cellmap/cellmap/zouinkhim/cellmap-flow/example/model_spec.py"
-dataset = "/groups/cellmap/cellmap/ackermand/for_hideo/jrc_pri_neuron_0710Dish4/jrc_pri_neuron_0710Dish4.n5/em/fibsem-uint8/s0"
 
-model_config = ScriptModelConfig(script_path=script_path)
-server = CellMapFlowServer(dataset, model_config)
-# %%
-chunk_x = 2
-chunk_y = 2
-chunk_z = 2
+def server_check(script_path, dataset):
+    model_config = ScriptModelConfig(script_path=script_path)
+    server = CellMapFlowServer(dataset, model_config)
+    chunk_x = 2
+    chunk_y = 2
+    chunk_z = 2
 
-server.chunk(None, None, chunk_x, chunk_y, chunk_z, None)
-# %%
+    server._chunk_impl(None, None, chunk_x, chunk_y, chunk_z, None)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Test run a CellMapFlow server")
+    parser.add_argument(
+        "--script_path",
+        "-s",
+        type=str,
+        help="Path to the Python script containing model specification",
+    )
+    parser.add_argument("--dataset", "-d", type=str, help="Path to the dataset")
+    args = parser.parse_args()
+    server_check(args.script_path, args.dataset)
