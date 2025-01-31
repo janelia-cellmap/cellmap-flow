22c0ddcf3276e1a430c903e8eeb0e2b040c79ff9
Marwan Zouinkhi <zouinkhi.marwan@gmail.com>
2025-01-31 16:44:22 -0500

    inject code

--- example/example_norm/custom_code_20250131_164003.py
+++
@@ -1,9 +0,0 @@
-class ZScoreNormalizer(InputNormalizer):
-
-    def __init__(self, mean=0.0, std=1.0):
-        self.mean = mean
-        self.std = std
-
-    def normalize(self, data: np.ndarray) -> np.ndarray:
-        data = data.astype(np.float32)
-        return ((data - self.mean) / self.std).astype(np.float32)
