616d86052f7203504f7ca53097831adcade81872
Marwan Zouinkhi <zouinkhi.marwan@gmail.com>
2025-02-24 16:55:06 -0500

    postprocess

--- cellmap_flow/dashboard/app.py
+++ cellmap_flow/dashboard/app.py
@@ -4,7 +4,7 @@ import neuroglancer
 from datetime import datetime
 from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
-
+import logging
 from cellmap_flow.utils.web_utils import get_free_port
 from cellmap_flow.norm.input_normalize import (
     get_input_normalizers,
@@ -16,6 +16,7 @@ from cellmap_flow.utils.scale_pyramid import get_raw_layer
 from cellmap_flow.utils.web_utils import encode_to_str, decode_to_json, ARGS_KEY, INPUT_NORM_DICT_KEY, POSTPROCESS_DICT_KEY
 import cellmap_flow.globals as g
 
+logger = logging.getLogger(__name__)
 app = Flask(__name__)
 CORS(app)
 NEUROGLANCER_URL = None
@@ -29,23 +30,34 @@ def index():
     input_norms = get_input_normalizers()
     output_postprocessors = get_postprocessors_list()
 
+    default_post_process = {d.to_dict()["name"]: d.to_dict() for d in g.postprocess}
+    default_input_norm = {d.to_dict()["name"]: d.to_dict() for d in g.input_norms}
+    logger.warning(f"Default postprocess: {default_post_process}")
+    logger.warning(f"Default input norm: {default_input_norm}")
+
     return render_template(
         "index.html",
         neuroglancer_url=NEUROGLANCER_URL,
         inference_servers=INFERENCE_SERVER,
         input_normalizers=input_norms,
-        output_postprocessors = output_postprocessors,
+        output_postprocessors=output_postprocessors,
+        default_post_process=default_post_process,
+        default_input_norm=default_input_norm  # Pass it here
     )
 
+def is_output_segmentation():
+    if len(g.postprocess) > 0 and g.postprocess[-1].is_segmentation:
+        return True
+    return False
 
 @app.route("/api/process", methods=["POST"])
 def process():
     data = request.get_json()
-    print(f"Data received: {type(data)} - {data.keys()} -{data}", flush=True)
+    logger.warning(f"Data received: {type(data)} - {data.keys()} -{data}")
     custom_code = data.get("custom_code", None)
     if "custom_code" in data:
         del data["custom_code"]
-    print(f"Data received: {type(data)} - {data.keys()} -{data}", flush=True)
+    logger.warning(f"Data received: {type(data)} - {data.keys()} -{data}")
     g.input_norms = get_normalizations(data["input_norm"])
     g.postprocess = get_postprocessors(data["postprocess"])
 
@@ -59,11 +71,16 @@ def process():
             # response = requests.post(f"{host}/input_normalize", json=data)
             # print(f"Response from {host}: {response.json()}")
             st_data = encode_to_str(data)
-            s.layers[model] = neuroglancer.ImageLayer(
-                source=f"n5://{host}/{model}{ARGS_KEY}{st_data}{ARGS_KEY}",
-            )
+            if is_output_segmentation():
+                s.layers[model] = neuroglancer.SegmentationLayer(
+                    source=f"n5://{host}/{model}{ARGS_KEY}{st_data}{ARGS_KEY}",
+                )
+            else:
+                s.layers[model] = neuroglancer.ImageLayer(
+                    source=f"n5://{host}/{model}{ARGS_KEY}{st_data}{ARGS_KEY}",
+                )
 
-    print(f"Input normalizers: {g.input_norms}", flush=True)
+    logger.warning(f"Input normalizers: {g.input_norms}")
 
     if custom_code:
 
@@ -77,12 +94,12 @@ def process():
                 file.write(custom_code)
 
             config = load_safe_config(filepath)
-            print(f"Custom code loaded successfully: {config}")
+            logger.warning(f"Custom code loaded successfully: {config}")
 
-            print(get_input_normalizers())
+            logger.warning(get_input_normalizers())
 
         except Exception as e:
-            print(f"Error executing custom code: {e}")
+            logger.warning(f"Error executing custom code: {e}")
 
     return jsonify(
         {
@@ -99,7 +116,7 @@ def create_and_run_app(neuroglancer_url=None, inference_servers=None):
     INFERENCE_SERVER = inference_servers
     hostname = socket.gethostname()
     port = get_free_port()
-    print(f"Host name: {hostname}", flush=True)
+    logger.warning(f"Host name: {hostname}")
     app.run(host="0.0.0.0", port=port, debug=False, use_reloader=False)
 
 
--- cellmap_flow/post/postprocessors.py
+++ cellmap_flow/post/postprocessors.py
@@ -41,14 +41,21 @@ class PostProcessor:
     @property
     def dtype(self):
         return np.uint8
+    
+    @property
+    def is_segmentation(self):
+        return False
 
 class DefaultPostprocessor(PostProcessor):
-    def __init__(self):
-        pass
+    def __init__(self, clip_min: float = -1.0, clip_max: float = 1.0,bias: float = 1.0, multiplier: float = 127.5):
+        self.clip_min = float(clip_min)
+        self.clip_max = float(clip_max)
+        self.bias = float(bias)
+        self.multiplier = float(multiplier)
 
     def _process(self, data):
-        data = data.clip(-1, 1)
-        data = (data + 1) * 255.0 / 2.0
+        data = data.clip(self.clip_min, self.clip_max)
+        data = (data + self.bias) * self.multiplier
         return data.astype(np.uint8) 
     
     def to_dict(self):
@@ -57,6 +64,50 @@ class DefaultPostprocessor(PostProcessor):
     @property
     def dtype(self):
         return np.uint8
+    
+class ThresholdPostprocessor(PostProcessor):
+    def __init__(self, threshold: float = 0.5):
+        self.threshold = float(threshold)
+
+    def _process(self, data):
+        data = (data.astype(np.float32) > self.threshold).astype(np.uint8)
+        return data
+
+    def to_dict(self):
+        return {"name": self.name(), "threshold": self.threshold}
+
+    @property
+    def dtype(self):
+        return np.uint8
+    
+    @property
+    def is_segmentation(self):
+        return True
+
+from scipy.ndimage import label
+class LabelPostprocessor(PostProcessor):
+    def __init__(self, channel:int = 0):
+        self.channel = int(channel)
+
+    def _process(self, data):
+        to_process = data[self.channel] 
+
+            
+        to_process, num_features = label(to_process)
+        data[self.channel] = to_process
+        return data
+    
+    def to_dict(self):
+        return {"name": self.name()}
+    
+    @property
+    def dtype(self):
+        return np.uint8
+    
+    @property
+    def is_segmentation(self):
+        return True
+
 
 
 
