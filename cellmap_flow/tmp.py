c6e291c3445987e37b301b2971b623d7879aa2a1
Marwan Zouinkhi <zouinkhi.marwan@gmail.com>
2025-07-15 12:47:18 +0200

    add create option

---
+++ cellmap_flow/tmp.py
@@ -0,0 +1,7 @@
+# %%
+dataset_name = (
+    "/Users/zouinkhim/Desktop/cellmap/cellmap-flow/tests/script_test/dummy.zarr/raw"
+)
+from cellmap_flow.image_data_interface import ImageDataInterface
+
+x = ImageDataInterface(dataset_name)
