36f64a94088d93a34830c12be993c7f0fa0536f0
Marwan Zouinkhi <zouinkhi.marwan@gmail.com>
2025-07-19 12:22:19 -0400

    remove tmp

--- cellmap_flow/tmp.py
+++
@@ -1,7 +0,0 @@
-# %%
-dataset_name = (
-    "/Users/zouinkhim/Desktop/cellmap/cellmap-flow/tests/script_test/dummy.zarr/raw"
-)
-from cellmap_flow.image_data_interface import ImageDataInterface
-
-x = ImageDataInterface(dataset_name)
