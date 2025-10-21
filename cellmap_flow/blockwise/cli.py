b1215e3c939f3db306b85594cf094363a514ac2b
zouinkhim <zouinkhi.marwan@gmail.com>
2025-10-21 15:25:08 -0400

    easier cli

--- cellmap_flow/blockwise/cli.py
+++ cellmap_flow/blockwise/cli.py
@@ -4,30 +4,8 @@ import logging
 from cellmap_flow.blockwise import CellMapFlowBlockwiseProcessor
 
 
-@click.group()
-@click.option(
-    "--log-level",
-    type=click.Choice(
-        ["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"], case_sensitive=False
-    ),
-    default="INFO",
-)
-def cli(log_level):
-
-    logging.basicConfig(level=getattr(logging, log_level.upper()))
-
-
-logger = logging.getLogger(__name__)
-
-
-@cli.command()
-@click.option(
-    "-y",
-    "--yaml_config",
-    required=True,
-    type=click.Path(exists=True),
-    help="The path to the YAML file.",
-)
+@click.command()
+@click.argument("yaml_config", type=click.Path(exists=True))
 @click.option(
     "-c",
     "--client",
@@ -35,10 +13,22 @@ logger = logging.getLogger(__name__)
     default=False,
     help="Run as client if this flag is set.",
 )
-def run(yaml_config, client):
+@click.option(
+    "--log-level",
+    type=click.Choice(
+        ["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"], case_sensitive=False
+    ),
+    default="INFO",
+)
+def cli(yaml_config, client, log_level):
+    logging.basicConfig(level=getattr(logging, log_level.upper()))
+
     is_server = not client
     process = CellMapFlowBlockwiseProcessor(yaml_config, create=is_server)
     if is_server:
         process.run()
     else:
         process.client()
+
+
+logger = logging.getLogger(__name__)
